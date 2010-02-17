8d3ffb055678ae0a0304347cb0561c5e584627e2
impetus.opensource <impetus.opensource@14a4b5af-f354-0410-80a2-f32ac1970506>
2010-02-17 07:20:04 +0000

    Copyright 2008 Impetus Infotech. added

--- zing/ZingWeb/test/com/spice/exception/TestException.java
+++ zing/ZingWeb/test/com/spice/exception/TestException.java
@@ -1,5 +1,5 @@
    /*
-    * Copyright 2008 The Apache Software Foundation.
+    * Copyright 2008 Impetus Infotech.
     *
     * Licensed under the Apache License, Version 2.0 (the "License");
     * you may not use this file except in compliance with the License.
